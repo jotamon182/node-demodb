281e7dd528e075ec7e1599e82a7c3b7b98b51884
Juan Munoz <juangui890@hotmail.com>
2020-10-20 13:00:23 -0300

    cambios basicos

--- demodb.js
+++ demodb.js
@@ -3,16 +3,17 @@ const express   = require('express')
 const app       = express()
 const port      = 3000
 
-var pagina_demodb   = 'http://localhost/demodb_chrome/';
+// var pagina_demodb   = 'http://localhost/demodb_chrome/';
+var pagina_demodb   = 'http://demodb.jmunoz.xyz/index.php';
 var ruts = ['88345400','28518775','65124087']
 
 
 async function inicio (){
     let browser = await puppeteer.launch({headless:false});
-    await honorarios(browser);
-}
-async function honorarios (browser){
     const new_page = await browser.newPage();
+    await honorarios(new_page);
+}
+async function honorarios (new_page){
     await new_page.goto(pagina_demodb);
     var valor = ruts.shift().toString();
     // await new_page.evaluate( async () => {
--- index.js
+++ index.js
@@ -30,3 +30,10 @@ const puppeteer = require('puppeteer');
   }
   await browser.close();
 })();
+
+https://blog.ramosly.com/how-ive-been-using-puppeteer-b8010e374ff7
+https://pptr.dev/#?product=Puppeteer&version=v5.3.1&show=api-pagetitle
+https://pptr.dev/#?product=Puppeteer&version=v5.3.1&show=api-class-page
+https://pocketadmin.tech/en/puppeteer-popup-window/
+https://stackoverflow.com/questions/48218584/tell-puppeteer-to-open-chrome-tab-instead-of-window
+https://github.com/jotamon182/node-demodb
